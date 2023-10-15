0596a7497ab2d81b00e2b9a5cfe80932affb74c6
Ana Clara <ananacarantes@gmail.com>
2023-10-15 20:03:53 -0300

    Função volcões

--- api.js
+++ api.js
@@ -9,15 +9,25 @@ async function run() {
   try {
     const args_uv = {
       'Content-Type': 'application/json',
-      'x-access-token': 'key'
+      'x-access-token': 'openuv-key-io'
     };
+    const urlVulcoes = "https://raw.githubusercontent.com/AnaArantesBarros/GeosenseAPI/main/vulcoes.json";
     const args_nasa = "PRECTOT,T2M,T2M_MAX,T2M_MIN";
     const urlAltitude = [`https://api.opentopodata.org/v1/srtm30m?locations=${LAT},${LON}`,`https://api.opentopodata.org/v1/srtm30m?locations=${LAT},${LON + 0.001}`];
+    const vulcoes = await obterTitulosEventos(urlVulcoes);
+    var coordenadas = [];
+    var status = [];
+
+    for (var vulcao in vulcoes) {
+      const vulcaoData = vulcoes[vulcao];
+      const coord = vulcaoData.Coordenada;
+      //const nomedovulcao = vulcaoData.Vulcao;  
+      var status = estaDentroDaDistancia(coord, [LAT, LON]);
+    }
     const altitudesPromises = urlAltitude.map(getAltitude);
     const altitudes = await Promise.all(altitudesPromises);
-    //const uvData = await getUv(args_uv, LAT, LON);
     const nasaData = await nasa(dataPassada, dataAtual, LAT, LON, args_nasa);
-    console.log(nasaData,slope(altitudes));
+    console.log(nasaData,slope(altitudes),status);
   } catch (error) {
     console.error('Erro:', error.message);
   }
@@ -100,12 +110,14 @@ async function nasa(data_inicio, data_fim, lat, lon, args) {
     throw new Error(`Erro na requisição da NASA: ${error.message}`);
   }
 }
+
+
 // Funções para cálculo do slope
 async function getAltitude(url) {
   try {
     const response = await fetch(url);
     if (!response.ok) {
-      throw new Error('Erro ao obter dados de UV');
+      throw new Error('Erro ao obter altitude');
     }
     const data = await response.json();
     return data.results[0].elevation;
@@ -119,5 +131,51 @@ function slope(altitudes) {
   const slope = (deltaElevation/ 111.32) * 100
   return Math.abs(slope.toFixed(2))
 }
-    
+
+// Função para verificar vulcões próximos
+async function obterTitulosEventos(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Erro ao obter dados de vulcoes');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error(`Erro na requisição de vulcoes: ${error.message}`);
+  }
+}
+
+function estaDentroDaDistancia(coord1, coord2, distanciaMaxKm = 10) {
+  // Coordenadas no formato (latitude, longitude)
+  const [lat1, lon1] = coord1;
+  const [lat2, lon2] = coord2;
+
+  // Raio da Terra em km
+  const raioTerraKm = 6371.0;
+
+  // Converter coordenadas de graus para radianos
+  const lat1Rad = toRadians(lat1);
+  const lon1Rad = toRadians(lon1);
+  const lat2Rad = toRadians(lat2);
+  const lon2Rad = toRadians(lon2);
+
+  // Diferença das longitudes
+  const deltaLon = lon2Rad - lon1Rad;
+
+  // Fórmula de Haversine
+  const a = Math.sin((lat2Rad - lat1Rad) / 2) ** 2 + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.sin(deltaLon / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  // Distância em quilômetros
+  const distanciaKm = raioTerraKm * c;
+
+  // Verifica se a distância está dentro do limite máximo (10 km)
+  return distanciaKm <= distanciaMaxKm;
+}
+
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
 run(); // Chama a função para executar o código assíncrono
